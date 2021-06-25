f9ca5a20fa1c665ee9c252d270d0675b8b442600
charliekranz <charliekranz3@gmail.com>
2021-06-25 10:28:09 +0000

    Corrected issue in views checkout

--- checkout/views.py
+++ checkout/views.py
@@ -8,12 +8,12 @@ def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
-        return redirect(revers('products'))
+        return redirect(reverse('products'))
 
-        order_form = OrderForm()
-        template = 'checkout/checkout.html'
-        context = {
-            'orders_form': order_form,
-        }
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
 
-        return render(request, template, context)
+    return render(request, template, context)
