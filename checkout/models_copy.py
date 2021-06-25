6119b85be4cfaa13a458028f3b2249978e5512fa
charliekranz <charliekranz3@gmail.com>
2021-06-25 16:53:56 +0000

    found issue in models py

--- checkout/models_copy.py
+++ checkout/models_copy.py
@@ -23,16 +23,17 @@ class Order(models.Model):
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
+
     def _generate_order_number(self):
         """
         Generate a random, unique order number using UUID
         """
-        return uuid.uuid4().hex.upper()
+        return uuid.uuid4().hex.upper
 
     def update_total(self):
         """
         Update grand total each time a line item is added,
-        accounting for delivery costs.
+        accounting for delivery costs
         """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
@@ -45,14 +46,14 @@ class Order(models.Model):
     def save(self, *args, **kwargs):
         """
         Override the original save method to set the order number
-        if it hasn't been set already.
+        if it hasn't been set already
         """
         if not self.order_number:
             self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.order_number
+        return self.order_name
 
 
 class OrderLineItem(models.Model):
@@ -64,8 +65,8 @@ class OrderLineItem(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Override the original save method to set the lineitem total
-        and update the order total.
+        Override the original save method to set the order number
+        and update the order total
         """
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
